4fcec24b085c678c3a22c6e3bb3af62bff91e072
wsilverio <wendeurick@gmail.com>
2014-10-19 16:09:58 -0200

    comentarios atualizados

--- PET.py
+++ PET.py
@@ -1,4 +1,4 @@
 P_M = '##########' # substitua pelo e-mail principal do PET * este endereço deve estar vinculado à conta do Gmail
-Sis_M = '########' # substitua pelo g-mail da conta Google
+Sis_M = '########' # substitua pelo gmail da conta Google
 Sis_S = '########' # substitua pela senha da conta Google
 KEY_BD = '#######' # substitua pela chave *key* da planilha "Banco de Dados"
